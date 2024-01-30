c7173dd251a675835cbc2245d1c0525564f27998
Hestaine <ariffhestaine@gmail.com>
2024-01-30 14:32:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+// Challenge 1
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markMass = 78;
+const johnMass = 92;
+
+const markBmi = markMass / markHeight ** 2;
+const johnBmi = johnMass / johnHeight ** 2;
+
+console.log(markBmi);
+console.log(johnBmi);
+
+const markHigherBMI = markBmi > johnBmi;
+
+console.log(markHigherBMI);
+*/
