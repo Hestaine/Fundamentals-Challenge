ce0ebe0c062851d4d8eed6471c21deea0cef4e8c
Hestaine <ariffhestaine@gmail.com>
2024-02-05 15:12:45 +0100

    yohohoho I'm done

--- script.js
+++ script.js
@@ -63,4 +63,11 @@ if (
 } else {
   console.log("Sorry there's no winner");
 }
+
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `The bill is ${bill}, the tip is ${tip}, and the total value is ${bill + tip}`
+);
 */
